23ac08038bc5e983fb1efca6198b03afb8d7d08c
majifo99 <135087859+majifo99@users.noreply.github.com>
2024-05-21 15:10:04 -0600

    First commit

---
+++ src/types/Types.ts
@@ -0,0 +1,28 @@
+export type User = {
+UserId: number,
+Name:string,
+PhoneNumber: string,
+Email:string,
+Password: string
+
+}
+
+export type Role= {
+RoleID:number,
+Name:string
+
+}
+
+export type clinic={
+ClinicID: number,
+Name: string
+Address:string
+}
+
+export type Appointment ={
+AppointmentID: number,
+DateTime: Date;
+Location: string;
+Status: string;
+AppointmentType: string;
+} 
